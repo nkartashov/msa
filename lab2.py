f4a91752395b6dbcde3b236d3e67591d44a7bf76
Nikita Kartashov <snailandmail@gmail.com>
2015-03-20 20:12:12 +0300

    Added code for baum-welch and results

--- lab2.py
+++ lab2.py
@@ -3,11 +3,13 @@
 
 from __future__ import division
 
-from math import log, exp, log10
-from Bio import SeqIO
-from Bio.SeqUtils import GC
+from math import log, exp
+from decimal import Decimal
 import sys
+
+from Bio import SeqIO
 from blist import blist
+
 
 ALPHABET = 'acgt'
 
@@ -49,8 +51,16 @@ def pretty_p(p):
     return '{0:.4f}'.format(p)
 
 
+def pretty_scientific_p(p):
+    return '{0:.4e}'.format(p)
+
+
 def pretty_e_p(p):
-    return pretty_p(10 ** p)
+    return pretty_p(exp(p))
+
+
+def pretty_scientific_e_p(p):
+    return pretty_scientific_p(exp(p))
 
 
 def run_viterbi(emission_p, sequence, start_p, transition_p):
@@ -103,7 +113,7 @@ def run_viterbi(emission_p, sequence, start_p, transition_p):
                     segment_statistics[new_state] += 1
                 else:
                     transition_statistics[state][new_state] += 1
-            transition_statistics = [[log10(value) - log10(state_statistics[k]) for value in row] for k, row in
+            transition_statistics = [[log(value) - log(state_statistics[k]) for value in row] for k, row in
                                      enumerate(transition_statistics)]
             return state_statistics, segment_statistics, transition_statistics, gc_rich_segments
 
@@ -117,13 +127,89 @@ def run_viterbi(emission_p, sequence, start_p, transition_p):
             print('States: AT-rich = {0}\tGC-rich = {1}'.format(*state_statistics))
             print('Segments: AT-rich = {0}\tGC-rich = {1}'.format(*segment_statistics))
             for row in transition_statistics:
-                print('{0}\t{1}'.format(*map(pretty_e_p, row)))
+                print('\t'.join(pretty_e_p(value) for value in row))
+
         report()
     print(gc_rich_segments)
 
 
+CONVERGENCE_LIKELYHOOD_INCREASE = Decimal(0.1)
+
+
 def run_baum_welch(emission_p, sequence, start_p, transition_p):
-    return
+    rounds = 0
+    length = len(sequence)
+    log_likelyhood = -10050000000000
+    while True:
+        rounds += 1
+        alpha = [[0 for _ in STATES] for _ in xrange(length)]
+        beta = [[0 for _ in STATES] for _ in xrange(length)]
+
+        for state in STATES:
+            alpha[0][state] = start_p[state] + emission_p[state][sequence[0]]
+
+        for i in xrange(1, len(sequence)):
+            nucleotide = sequence[i]
+            for j in STATES:
+                alpha[i][j] = emission_p[j][nucleotide] + iter_slog(
+                    alpha[i - 1][state] + transition_p[state][j] for state in STATES)
+
+        for state in STATES:
+            beta[length - 1][state] = 1
+
+        for i in (length - j - 1 for j in xrange(1, len(sequence))):
+            nucleotide = sequence[i + 1]
+            for j in STATES:
+                beta[i][j] = iter_slog(
+                    beta[i + 1][state] + transition_p[j][state] + emission_p[j][nucleotide] for state in STATES)
+
+        gamma = [[0 for _ in STATES] for _ in xrange(length)]
+        ksi = [[[0 for _ in STATES] for _ in STATES] for _ in xrange(length - 1)]
+        sums = [0 for _ in xrange(length)]
+        for i in xrange(length):
+            for j in STATES:
+                gamma[i][j] = alpha[i][j] + beta[i][j]
+            sums[i] = iter_slog(gamma[i])
+            for j in STATES:
+                gamma[i][j] -= sums[i]
+
+        for i in xrange(length - 1):
+            nucleotide = sequence[i + 1]
+            for prev in STATES:
+                for next in STATES:
+                    ksi[i][prev][next] = alpha[i][prev] + beta[i + 1][next] + transition_p[prev][next] + \
+                                         emission_p[next][nucleotide] - sums[i]
+
+        start_p = gamma[0]
+        for i in STATES:
+            for j in STATES:
+                transition_p[i][j] = iter_slog(ksi[k][i][j] for k in xrange(length - 1)) - iter_slog(
+                    gamma[k][i] for k in xrange(length - 1))
+
+        for state in STATES:
+            for nucleotide in ALPHABET:
+                emission_p[state][nucleotide] = iter_slog(
+                    gamma[k][state] for k in xrange(length) if sequence[k] == nucleotide) - iter_slog(
+                    gamma[k][state] for k in xrange(length))
+
+        new_log_likelyhood = iter_slog(alpha[length - 1])
+        if new_log_likelyhood < log_likelyhood:
+            print('BAD STUFF')
+            break
+        else:
+            print(new_log_likelyhood)
+            if new_log_likelyhood - log_likelyhood < CONVERGENCE_LIKELYHOOD_INCREASE and rounds > 1:
+                print('CONVERGED!')
+                print('Took us {0} rounds'.format(rounds))
+                print('Transition probabilities:')
+                for row in transition_p:
+                    print('\t'.join(pretty_scientific_e_p(value) for value in row))
+                print('Emission probabilities:')
+                for row in emission_p:
+                    print('\t'.join('{0}: {1}'.format(key, pretty_scientific_e_p(value))
+                                    for key, value in row.iteritems()))
+                break
+            log_likelyhood = new_log_likelyhood
 
 
 def main():
@@ -134,11 +220,11 @@ def main():
     filename = sys.argv[1]
     if len(sys.argv) == 3:
         sys.stdout = open(sys.argv[2], 'w')
-    start_p = list(map(log10, [0.996, 0.004]))
-    transition_p = [list(map(log10, [0.999, 0.001])),
-                    list(map(log10, [0.01, 0.99]))]
-    emission_p = [{'a': log10(0.291), 'c': log10(0.209), 'g': log10(0.209), 't': log10(0.291)},
-                  {'a': log10(0.169), 'c': log10(0.331), 'g': log10(0.331), 't': log10(0.169)}]
+    start_p = list(map(log, [0.996, 0.004]))
+    transition_p = [list(map(log, [0.999, 0.001])),
+                    list(map(log, [0.01, 0.99]))]
+    emission_p = [{'a': log(0.291), 'c': log(0.209), 'g': log(0.209), 't': log(0.291)},
+                  {'a': log(0.169), 'c': log(0.331), 'g': log(0.331), 't': log(0.169)}]
 
     with open(filename) as handle:
         sequence = SeqIO.read(handle, 'fasta').seq.lower()
